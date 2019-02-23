0fcb8d24770cf2e7e1673f36e7a821deb3d25b3f
GilMuzika <47449752+GilMuzika@users.noreply.github.com>
2019-02-23 18:15:14 +0200

    Add files via upload

---
+++ ex1_odd_or_even.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Write a program, which compares correctly two real numbers with accuracy at least 0.000001.
+ */ 
+
+namespace Fundamentals_of_computer_programming_with_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number:");
+            int n;
+        EnterNumber:
+            if (Int32.TryParse(Console.ReadLine(), out n)) { }
+            else { Console.WriteLine("This is nit a numbeer. \nPlease try again: \n"); goto EnterNumber; }
+
+            Console.WriteLine(n%2 == 0 ? "Even" : "Odd");
+        }
+    }
+}
---
+++ ex2_divisible_by_both_5_and_7.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Write a Boolean expression that checks whether a given integer is divisible by both 5 and 7, without a remainder.
+ */
+
+namespace Fundamentals_of_computer_programming_with_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // function 'PleaseEnterSomeNumbers()' used for taking a number from the user input. 
+            // This function returns an array so accsess to the first (and in this case only) member of the aray 
+            // taken by indexation. The parameter that the function takes when called defines the number of its returned array members.
+            int n = PleaseEnterSomeNumbers(1)[0];
+
+             if (!(n % 5 == 0 && n % 7 == 0)) { Console.WriteLine("The number {0} can't be divided by 5 or 7 without a reminder.", n); }
+             else { Console.WriteLine("The number {0} can be divided by 5 or 7 without a reminder. \nIt's division by 5 is {1} and by 7 is {2}", n, n/5, n/7); }           
+        }
+
+
+
+
+
+
+        static int[] PleaseEnterSomeNumbers(int iterations)
+        {
+
+            if (iterations == 1) { Console.WriteLine("Please enter one number:\n"); }
+            else  { Console.WriteLine("Please enter {0} numbers:\n", iterations); }
+
+            int[] arriterations = new int[iterations];
+
+            for (int i = 0; i < iterations; i++) {
+                if (i > 0) { Console.Clear(); }
+                if (i != 0) { Console.WriteLine("Please enter a number: \n"); }
+                int line;
+                    EnterNumber:
+                    if (Int32.TryParse(Console.ReadLine(), out line)) { arriterations[i] = line; }
+                    else { Console.WriteLine("\n This is not a number! \nPlease enter only numbers. \nNow lets try again: \n"); goto EnterNumber; }
+            }
+
+            return arriterations;
+        }
+    }   
+}
+
+
+
+
+
